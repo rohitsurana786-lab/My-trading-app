f0db2706e3e2b7b8b6ee6e040cb23612248036fa
rohitsurana786-lab <rohitsurana786@gmail.com>
2026-09-14 10:36:57 +0530

    Update and rename Main.py to main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-code = '''
+cod
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
@@ -192,7 +192,6 @@ class MainApp(App):
 
 if __name__ == '__main__':
     MainApp().run()
-'''
 
 with open("main.py", "w") as f:
     f.write(code)
